449b49c1390803a2ce1b3e6ed971b58e57e610c1
Lotar <lotar.94@gmail.com>
2020-04-21 19:41:19 -0400

    Se utilizan servicios de la Nasa

--- KnowAboutUniverse/src/app/app.component.ts
+++ KnowAboutUniverse/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './services/data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  Data:Object = {};
+  constructor(private readonly DataServices:DataService){}
   title = 'KnowAboutUniverse';
+  ngOnInit(): void {
+    this.DataServices.getData().subscribe(res => {
+      this.Data= res
+      console.log('Hola desde la Naza! ', res);
+    });
+  }
+  
 }
